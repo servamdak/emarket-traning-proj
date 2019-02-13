b2d91acfc2cd73e10738c54977a41b7eb448b996
servamdak <servamdak@gmail.com>
2019-02-13 04:02:36 +0200

    Added POST method in ProductController, view resolver in configuration

--- src/main/java/com.market/configuration/WebConfiguration.java
+++ src/main/java/com.market/configuration/WebConfiguration.java
@@ -1,9 +1,11 @@
 package com.market.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
@@ -15,4 +17,11 @@ public class WebConfiguration implements WebMvcConfigurer {
                 .addResourceLocations("/resources/");
     }
 
+    @Bean
+    public InternalResourceViewResolver resourceViewResolver(){
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/resources/html/");
+        internalResourceViewResolver.setSuffix("");
+        return internalResourceViewResolver;
+    }
 }
--- src/main/java/com.market/controllers/ProductController.java
+++ src/main/java/com.market/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package com.market.controllers;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.reflect.Array;
@@ -13,6 +15,11 @@ public class ProductController {
     public List<String> products = Arrays.asList("Notebook", "Phone", "SmartTV", "Monitor", "Desktop", "SmartWatch", "Speaker");
 
     @GetMapping("/products")
+    public String outputProductPage(Model model){
+        return "home";
+    }
+
+    @PostMapping("/products")
     public List<String> outputAllProducts(){
         return products;
     }
