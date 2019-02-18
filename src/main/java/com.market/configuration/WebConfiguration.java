d46181d55151c9ee1d0111eca2c00c9fcad050e0
VV <servamdak@gmail.com>
2019-02-18 13:54:00 +0200

    Change WebConfiguration, added DispatcherServletContainerInitializer

--- src/main/java/com.market/configuration/WebConfiguration.java
+++ src/main/java/com.market/configuration/WebConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -14,14 +15,19 @@ public class WebConfiguration implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry) {
         resourceHandlerRegistry
                 .addResourceHandler("/resources/**")
-                .addResourceLocations("/resources/");
+                .addResourceLocations("/resources/static/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/products").setViewName("index");
     }
 
     @Bean
     public InternalResourceViewResolver resourceViewResolver(){
         InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
-        internalResourceViewResolver.setPrefix("/resources/html/");
-        internalResourceViewResolver.setSuffix("");
+        internalResourceViewResolver.setPrefix("/resources/static/");
+        internalResourceViewResolver.setSuffix(".html");
         return internalResourceViewResolver;
     }
 }
--- src/main/java/com.market/controllers/ProductController.java
+++ src/main/java/com.market/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package com.market.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,7 +10,7 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
-@RestController
+@Controller
 public class ProductController {
 
     public List<String> products = Arrays.asList("Notebook", "Phone", "SmartTV", "Monitor", "Desktop", "SmartWatch", "Speaker");
