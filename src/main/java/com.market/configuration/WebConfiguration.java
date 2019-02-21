5e991266fdc7376098209d58395edd4a59194983
servamdak <servamdak@gmail.com>
2019-02-21 05:31:57 +0200

    Added WebApplicationInitializer, changed WebConfiguration

--- src/main/java/com.market/configuration/WebConfiguration.java
+++ src/main/java/com.market/configuration/WebConfiguration.java
@@ -1,33 +1,60 @@
 package com.market.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.http.MediaType;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan
 public class WebConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private ContentNegotiationManager contentNegotiationManager;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry) {
         resourceHandlerRegistry
                 .addResourceHandler("/resources/**")
-                .addResourceLocations("/resources/static/");
+                .addResourceLocations("classpath:resources/static/");
     }
 
     @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/products").setViewName("index");
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        Map<String, MediaType> mediatypes = new HashMap<>();
+        mediatypes.put("html", MediaType.TEXT_HTML);
+//        mediatypes.put("pdf", MediaType.valueOf("application/pdf"));
+//        mediatypes.put("xls", MediaType.valueOf("application/vnd.ms-excel"));
+        mediatypes.put("xml", MediaType.APPLICATION_XML);
+        mediatypes.put("json", MediaType.APPLICATION_JSON);
+        configurer.mediaTypes(mediatypes);
+    }
+    @Bean
+    public ContentNegotiatingViewResolver contentNegotiatingViewResolver() {
+        ContentNegotiatingViewResolver viewResolver = new ContentNegotiatingViewResolver();
+        viewResolver.setContentNegotiationManager(contentNegotiationManager);
+        return viewResolver;
     }
 
-    @Bean
-    public InternalResourceViewResolver resourceViewResolver(){
-        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
-        internalResourceViewResolver.setPrefix("/resources/static/");
-        internalResourceViewResolver.setSuffix(".html");
-        return internalResourceViewResolver;
-    }
+//    @Bean
+//    public InternalResourceViewResolver resourceViewResolver(){
+//        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+//        internalResourceViewResolver.setPrefix("/resources/");
+//        internalResourceViewResolver.setSuffix(".*");
+//        return internalResourceViewResolver;
+//    }
+//
+//    @Override
+//    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//        configurer.enable();
+//    }
 }
